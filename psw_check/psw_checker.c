b739ab7441856c9907eb3e0a0a50195011d744c2
Manuel Romei <kriive@havce.it>
2022-12-13 00:17:18 +0100

    dockerize psw_check

--- psw_check/psw_checker.c
+++ psw_check/psw_checker.c
@@ -1,44 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#define DIM_VARIABLE 20
 
-void stamp_flag(){
-	 FILE *file = fopen("flag.txt", "r");
-	    if (file == NULL) {
-	        printf("Errore nell'apertura del file!\n");
-	        return 0;
-	    }
+#define BUF_LEN 20
 
-	    // leggi il file riga per riga e stampa il contenuto a video
-	    char line[100];
-	    while (fgets(line, 100, file) != NULL) {
-	        printf("%s", line);
-	    }
-
-	    // chiudi il file
-	    fclose(file);
+void setup() {
+  setvbuf(stdout, NULL, _IONBF, 0);
+  setvbuf(stderr, NULL, _IONBF, 0);
+  setvbuf(stdin, NULL, _IONBF, 0);
 }
-int main(void){
 
-	char name [DIM_VARIABLE];
-	char psw[DIM_VARIABLE];
-	do{
-		puts("Inserisci il tuo username:");
-		fgets(name, DIM_VARIABLE, stdin);
-	    name[strlen(name) - 1] = '\0';
-		puts("Inserisci la tua password:");
-		fgets(psw, DIM_VARIABLE, stdin);
-	    psw[strlen(psw) - 1] = '\0';
+void print_flag() {
+  char *flag = getenv("FLAG");
+  if (flag == NULL) {
+    printf("Error! If you see this error, please contact the admin.");
+    return;
+  }
 
-	    if(strcmp(name,"admin")==0 && strcmp(psw ,"lmE5apcAiF")==0){
-		   puts("LOGIN ESEGUITO ECCO LA TUA FLAG! ");
-		   stamp_flag();
-		   break;
-	    } else{
-	    	puts("Credenziali errate ! ");
-	    }
-	}while(1);
-return 0;
+  printf("Your flag is: %s\n", flag);
+}
+
+int main(void) {
+  char name[BUF_LEN];
+  char psw[BUF_LEN];
+  setup();
+
+  do {
+    puts("Inserisci il tuo username:");
+    fgets(name, BUF_LEN, stdin);
+
+    name[strlen(name) - 1] = '\0';
+
+    puts("Inserisci la tua password:");
+    fgets(psw, BUF_LEN, stdin);
+
+    psw[strlen(psw) - 1] = '\0';
+
+    if (strcmp(name, "admin") == 0 && strcmp(psw, "lmE5apcAiF") == 0) {
+      puts("LOGIN ESEGUITO ECCO LA TUA FLAG! ");
+      print_flag();
+      break;
+    }
+
+    puts("Credenziali errate ! ");
+  } while (1);
+
+  return 0;
 }
